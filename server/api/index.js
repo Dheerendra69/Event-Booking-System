b87898374e70872a51454a3bcb67e4d67e6599bf
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-07-13 20:33:44 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:3000", "https://event-bookin.netlify.app"],
   })
 );
 
