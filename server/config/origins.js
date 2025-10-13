8f87bf44326f05038226c1847360944ecd5b2d5f
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-10-13 18:10:45 +0530

    Update origins.js

--- server/config/origins.js
+++ server/config/origins.js
@@ -1,7 +1,7 @@
 const origins = [
   "https://event-bookin.netlify.app",
-  "https://journey-compass.netlify.app",
   "https://event-bookin-system.vercel.app",
+  "https://journey-compass.netlify.app",
 ];
 
 module.exports = origins;
