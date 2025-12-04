6a9988f070b89c7c69138ff73d3236beb257d9f7
Dheerendra Pratap Singh <dheerendrapratapsingh1509@gmail.com>
2025-12-04 11:27:03 +0530

    Delete: deleted extra files

--- config/origins.js
+++
@@ -1,7 +0,0 @@
-const origins = [
-  "https://event-bookin.netlify.app",
-  "https://event-bookin-system.vercel.app",
-  "https://journey-compass.netlify.app",
-];
-
-module.exports = origins;
--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect, isAdmin } = require("../middlewares/authMiddleware");
-
-router.get("/dashboard", protect, isAdmin, (req, res) => {
-  res.json({ message: "Welcome, Admin!" });
-});
-
-module.exports = router;
