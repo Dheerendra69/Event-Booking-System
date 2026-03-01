2095e0ae1e8884b537fafb36156541a133375cf6
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2026-03-01 11:19:47 +0530

    Reverted database connection

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,21 +1,22 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
-const db = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-(async () => {
-  try {
-    const connection = await db.getConnection();
-    console.log("Database connected successfully");
-    connection.release();
-  } catch (error) {
-    console.error("Database connection failed:", error);
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ Error connecting to MySQL:", err);
+  } else {
+    console.log("✅ MySQL Database connected successfully!");
   }
-})();
+});
 
-module.exports = db;
+module.exports = connection;
